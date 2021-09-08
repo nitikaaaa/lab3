f04153482526bbaac494ebbdee559d86d2dc45a6
nitika <nitika.nb.0710@gmail.com>
2021-09-08 16:24:29 +0530

    version1

---
+++ q3.java
@@ -0,0 +1,56 @@
+public class q3 {
+    public static void main(String[] args) {
+        int month;
+        month = Integer.parseInt(args[0]);
+        if (month == 1) {
+            System.out.println("JANUARY");
+        } else if (month == 2) {
+            System.out.println("FEBURARY");
+        }
+
+        else if (month == 3) {
+            System.out.println("MARCH");
+        }
+
+        else if (month == 4) {
+            System.out.println("APRIL");
+        }
+
+        else if (month == 5) {
+            System.out.println("MAY");
+        }
+
+        else if (month == 6) {
+            System.out.println("JUNE");
+        }
+
+        else if (month == 7) {
+            System.out.println("JULY");
+        }
+
+        else if (month == 8) {
+            System.out.println("AUGUST");
+        }
+
+        else if (month == 9) {
+            System.out.println("SEPTEMBER");
+        }
+
+        else if (month == 10) {
+            System.out.println("OCTOBER");
+        }
+
+        else if (month == 11) {
+            System.out.println("NOVEMBER");
+        }
+
+        else if (month == 12) {
+            System.out.println("DECEMBER");
+        }
+
+        else {
+            System.out.println("oops");
+        }
+
+    }
+}
